c205ba0c31009583208bc8fb10b8c750e20c8f0e
jlazaro <jlazaro135@gmail.com>
2023-03-25 11:28:25 +0100

    se continua desarrollo

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -30,12 +30,12 @@ const allAreChosen = ref(false)
 const chosenDestinies = ref([])
 
 function checkDestiniesChosen(chosenCode){
-  if(chosenDestinies.value.find(destiny => destiny.code === chosenCode)){
-    console.log(chosenDestinies.value.find(destiny => destiny.code === chosenCode))
-    console.log('ya existe, coge otro')
+  if(chosenDestinies.value.find(destiny => destiny.code === chosenCode.code)){
+    let indexOfDestiny = chosenDestinies.value.indexOf(chosenCode)
+    chosenDestinies.value.splice(indexOfDestiny, 1 )
     return
   }
-  chosenDestinies.value.push(getDestinies.value.find(destiny => destiny.code === chosenCode))
+  chosenDestinies.value.push(getDestinies.value.find(destiny => destiny.code === chosenCode.code))
   destinyRef.value = props.destinos - chosenDestinies.value.length
   if(chosenDestinies.value.length >= props.destinos){
     allAreChosen.value = true
@@ -49,22 +49,19 @@ const emit = defineEmits(['closeModal'])
 <template>
 <dialog :open="modalIsOpen">
   <article>
-    <h5>{{ nombre }} - destinos a seleccionar: {{ destinyRef }}</h5>
+    <h5 class="destinies-counter">{{ nombre }} - destinos a seleccionar: {{ destinyRef }}</h5>
     <main>
       <div class="accordion" id="accordionExample">
         <div class="accordion-item">
-          <h2 class="accordion-header" id="headingOne">
-            <button class="" :class="!allAreChosen ? '' : 'collapsed'" type="button" data-bs-toggle="collapse" data-bs-target="" aria-expanded="true" aria-controls="collapseOne">
-              Destinos a elegir
-            </button>
-          </h2>
+          <h3 class="heading">
+              Paso 1: Elegir destinos
+              <span style="font-size: 12px;">- Ordénalos en el siguiente paso</span>
+          </h3>
           <div id="collapseOne" class="accordion-collapse" :class="!allAreChosen ? 'collapse show' : 'collapse'" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-            <div class="accordion-body">
-                <div class="item-card" v-for="destiny in getDestinies" @click="checkDestiniesChosen(destiny.code)">
-                  <p>Destino: {{ destiny.destino }}</p>
-                  <p>Ciudad: {{ destiny.ciudad }}</p>
-                  <p>Código: {{ destiny.code }}</p>
-                </div>
+            <div class="item-card" v-for="destiny, index in getDestinies" @click="checkDestiniesChosen(destiny)" :class="{'is-selected': chosenDestinies.find(chosenDestiny => chosenDestiny.code === destiny.code)}">
+              <p class="item-p">Destino: {{ destiny.destino }}</p>
+              <p class="item-p">Ciudad: {{ destiny.ciudad }}</p>
+              <p class="item-p">Código: {{ destiny.code }}</p>
             </div>
           </div>
         </div>
@@ -78,11 +75,11 @@ const emit = defineEmits(['closeModal'])
             <div class="accordion-body">
                 <div class="item-card" v-for="destiny, index in chosenDestinies">
                   <div class="mb-1 p-1 text-center bg-info text-white fw-bold">Posición {{ index + 1 }}</div>
-                  <div>
-                    <p>Destino: {{ destiny.destino }}</p>
-                    <p>Ciudad: {{ destiny.ciudad }}</p>
-                    <p>Código: {{ destiny.code }}</p>
-                  </div>
+                    <div>
+                      <p>Destino: {{ destiny.destino }}</p>
+                      <p>Ciudad: {{ destiny.ciudad }}</p>
+                      <p>Código: {{ destiny.code }}</p>
+                    </div>
                   <div>
                     <a href="#">Eliminar</a>
                   </div>
@@ -125,31 +122,90 @@ max-width: 100%;
 }
 
 @media screen and (max-width: 992px) {
-  .box-wrapper{
+  .box-wrapper {
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
-}
-  .box{
+  }
+  .box {
     max-width: 100%;
   }
 }
 
-.item-card{
-  background-color: rgb(233, 233, 233);
+.destinies-counter{
+  position: fixed;
+  bottom: 25px;
+  left: 0;
+  right: 0;
+  width: fit-content;
+  max-width: 90%;
+  margin: 0 auto;
+  z-index: 9;
+  background-color: rgb(0, 4, 255);
+  color: white;
+  border-radius: 5px;
   padding: 0.5rem;
-  border: 3px solid transparent;
+  box-shadow: 0 0 10px lightgray
+}
+
+.item-card{
+  background-color: rgba(233, 233, 233, 0.521);
+  padding: 0.5rem;
+  border: 1px solid transparent;
   cursor: pointer;
-  margin: 1rem 0;
+}
+
+.item-p{
+  max-width: 80%;
 }
 
 .item-card:hover{
-  border-color: gray
+  box-shadow: 0 0 10px rgb(201, 201, 201)
 }
+
+.is-selected{
+  background-color: rgb(196, 255, 196);
+  border-color: darkgreen;
+  position: relative;
+  overflow: hidden;
+  z-index: 0;
+}
+
+.is-selected:after{
+  content: 'Elegido';
+    width: 193px;
+    height: 41px;
+    text-align: center;
+    position: absolute;
+    background-color: darkgreen;
+    padding: 0.5rem;
+    color: white;
+    top: 10px;
+    right: -59px;
+    transform: rotate(43deg);
+    font-size: 17px;
+}
+
 
 p{
   margin: 0;
   font-size: 14px;
+}
+
+.accordion-collapse{
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  border: 1px solid gray;
+  padding: 1rem;
+}
+
+.heading{
+  padding: 1rem;
+  background-color: lightcyan;
+  border: 1px solid gray;
+  border-bottom: 0;
+  margin: 0
 }
 </style>
   
--- src/js/destinies.js
+++ src/js/destinies.js
@@ -2,126 +2,138 @@ export const destinies = [
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Córdoba",
-        code: "12345asssd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Almería",
-        code: "1fsdf2345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Sevilla",
-        code: "12345asd",
+        code: makeid(5),
     },
     {
         destino: "Consejería de medio ambiente, sostenibilidad y economía azul",
         ciudad: "Granada",
-        code: "dfsdfsdw",
+        code: makeid(5),
     }
 ]
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
